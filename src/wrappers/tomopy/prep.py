880aa2aaf516ddc9e527f964c1c928f0561f00b2
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2022-10-26 13:11:30 +0100

    Fix normalisation processing in `simple_tomopy_pipeline.yaml`
    The use of the `tomopy.prep.normalize.minus_log()` function was missing
    in `simple_tomopy_pipeline.yaml` and thus causing the result to be
    incorrect.
    
    Adding in the use of this method revealed the need to have some logic to
    determine if a wrapped method in tomopy does or doesn't need the darks
    and flats as parameters.

--- src/wrappers/tomopy/prep.py
+++ src/wrappers/tomopy/prep.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from inspect import signature
 
 from numpy import ndarray
 from tomopy import prep
@@ -51,6 +52,13 @@ def normalize(params: Dict, method_name: str, data: ndarray, flats: ndarray,
         A numpy array of normalized projections.
     """
     module = getattr(prep, 'normalize')
-    data = getattr(module, method_name)(data, flats, darks, **params)
-    data[data == 0.0] = 1e-09
+    # Some functions in `tomopy.prep.normalize` need the flats and darks, but
+    # others do not, so this needs to be checked prior to passing the parameters
+    # to the wrapped tomopy function
+    func = getattr(module, method_name)
+    sig_params = signature(func).parameters
+    if 'dark' in sig_params and 'flat' in sig_params:
+        data = getattr(module, method_name)(data, flats, darks, **params)
+    else:
+        data = getattr(module, method_name)(data, **params)
     return data
