f2812d80a4fff91cf0167b150c4ef56f189653d3
Naman Gera <namangera15@gmail.com>
2023-02-13 17:19:17 +0000

    test the generated images

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,12 +1,37 @@
 import os
 import subprocess
 import sys
+from pathlib import Path
+from shutil import rmtree
+
+import numpy as np
+from PIL import Image
 
 if not os.path.exists("output_dir"):
     os.mkdir("output_dir/")
 
 
+def clean_folder():
+    for path in Path("output_dir").iterdir():
+        if path.is_file():
+            path.unlink()
+        elif path.is_dir():
+            rmtree(path)
+
+
+def read_folder(folder):
+    files = []
+    for f in os.listdir(folder):
+        f = os.path.join(folder, f)
+        if os.path.isdir(f):
+            files = [*files, *read_folder(f)]
+        else:
+            files.append(f)
+    return files
+
+
 def test_tomo_standard_loaded():
+    clean_folder()
     cmd = [
         sys.executable,
         "-m", "httomo",
@@ -23,3 +48,18 @@ def test_tomo_standard_loaded():
     assert "Running task 6 (pattern=sinogram): recon" in output
     assert "Running task 7 (pattern=all): save_to_images" in output
     assert "Total number of reslices: 1" in output
+
+    # recurse through output_dir and check that all files are there
+    files = read_folder("output_dir/")
+    assert len(files) == 5
+
+    # check the .tif files
+    tif_files = list(filter(lambda x: '.tif' in x, files))
+    assert len(tif_files) == 3
+    #: check that the image size is correct
+    imarray = np.array(Image.open(tif_files[0]))
+    assert imarray.shape == (160, 160)
+
+    #: check h5 files
+    h5_files = list(filter(lambda x: '.h5' in x, files))
+    assert len(h5_files) == 2
