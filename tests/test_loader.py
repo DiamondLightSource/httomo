336f9bfe303b31f75cd5b95c72446ca9edd8ed20
Naman Gera <namangera15@gmail.com>
2023-02-13 17:53:48 +0000

    Test `--save_all`

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -63,3 +63,32 @@ def test_tomo_standard_loaded():
     #: check h5 files
     h5_files = list(filter(lambda x: '.h5' in x, files))
     assert len(h5_files) == 2
+
+
+def test_tomo_standard_loaded_with_save_all():
+    clean_folder()
+    cmd = [
+        sys.executable,
+        "-m", "httomo",
+        "--save_all",
+        "testdata/tomo_standard.nxs",
+        "samples/pipeline_template_examples/testing_pipeline.yaml",
+        "output_dir/",
+        "task_runner",
+    ]
+    output = subprocess.check_output(cmd).decode().strip()
+    assert "Saving intermediate file: 2-tomopy-normalize-tomo.h5" in output
+    assert "Saving intermediate file: 3-tomopy-minus_log-tomo.h5" in output
+    assert "Saving intermediate file: 4-tomopy-remove_stripe_fw-tomo.h5" in output
+    assert "Saving intermediate file: 6-tomopy-recon-tomo-gridrec.h5" in output
+
+    files = read_folder("output_dir/")
+    assert len(files) == 8
+
+    # check the .tif files
+    tif_files = list(filter(lambda x: '.tif' in x, files))
+    assert len(tif_files) == 3
+
+    #: check h5 files
+    h5_files = list(filter(lambda x: '.h5' in x, files))
+    assert len(h5_files) == 5
