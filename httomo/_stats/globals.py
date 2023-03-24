edd13d61f707c0e1e7b377325cc9e6964793c38f
Daniil Kazantsev <dkazanc@hotmail.com>
2023-03-24 12:23:39 +0000

    changing/fixing tests

--- httomo/_stats/globals.py
+++ httomo/_stats/globals.py
@@ -5,6 +5,7 @@ from mpi4py.MPI import Comm
 gpu_enabled = False
 try:
     import cupy as xp
+
     try:
         xp.cuda.Device(0).compute_capability
         gpu_enabled = True  # CuPy is installed and GPU is available
@@ -13,14 +14,15 @@ try:
 except ImportError:
     import numpy as xp
 
+
 def min_max_mean_std(data: xp.ndarray, comm: Comm):
-    """ calculating global statistics of the given array
+    """calculating global statistics of the given array
     Args:
         data: (xp.ndarray): a numpy or cupy data array.
         comm: The MPI communicator to use.
     Returns:
         tuple[(float, float, float, float)]: (min, max, mean, std_var)
-    """    
+    """
     if isinstance(data, np.ndarray):
         data_not_numpyarray = False
     else:
@@ -30,9 +32,9 @@ def min_max_mean_std(data: xp.ndarray, comm: Comm):
     if gpu_enabled and data_not_numpyarray:
         converted = True
         data = xp.asnumpy(data)
-    
+
     data = np.nan_to_num(data, copy=False, nan=0.0, posinf=0, neginf=0)
-      
+
     comm.Barrier()
     data = data.flatten()
     # max/min
