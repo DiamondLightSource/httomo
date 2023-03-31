638d7c0a8d41361fc1d0a2d5cb83bc6497dc5c18
Naman Gera <namangera15@gmail.com>
2023-03-31 12:59:20 +0100

    Check only one method is being called from one module

--- httomo/yaml_checker.py
+++ httomo/yaml_checker.py
@@ -5,10 +5,10 @@ Module for checking the validity of yaml files.
 import yaml
 from httomo.yaml_utils import open_yaml_config
 
-__all__ = ['validate_yaml']
+__all__ = ["sanity_check", "check_one_method_per_module"]
 
 
-def validate_yaml(yaml_file):
+def sanity_check(yaml_file):
     """
     Check if the yaml file is properly indented, has valid mapping and tags.
     """
@@ -35,3 +35,34 @@ def validate_yaml(yaml_file):
                     f"Error in the YAML_CONFIG file at line {e.problem_mark.line}. "
                     "Please recheck the file."
                 )
+
+    return yaml_data
+
+
+def check_one_method_per_module(yaml_file):
+    """
+    Check that we cannot have a yaml file with more than one method
+    being called from one module. For example, we cannot have:
+
+    - tomopy.prep.normalize:
+        normalize:
+          data_in: tomo
+          data_out: tomo
+          cutoff: null
+        minus_log:
+          data_in: tomo
+          data_out: tomo
+    """
+    yaml_data = sanity_check(yaml_file)
+
+    lvalues = [value for d in yaml_data for value in d.values()]
+    for i, d in enumerate(lvalues):
+        assert isinstance(d, dict)
+        if len(d) != 1:
+            print(
+                f"More than one method is being called from the"
+                f" module '{next(iter(yaml_data[i]))}'. "
+                "Please recheck the yaml file."
+            )
+
+    return yaml_data
