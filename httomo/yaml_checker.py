e8c527d6f0ac9a9949fe8c761218fa9a3ed07f27
Naman Gera <namangera15@gmail.com>
2023-03-31 16:40:47 +0100

    use httomo.templates to validate the `YAML_CONFIG`

--- httomo/yaml_checker.py
+++ httomo/yaml_checker.py
@@ -1,18 +1,22 @@
 """
 Module for checking the validity of yaml files.
 """
-
+import os
 import yaml
 from httomo.yaml_utils import open_yaml_config
 
-__all__ = ["sanity_check", "check_one_method_per_module"]
+__all__ = [
+    "check_one_method_per_module",
+    "sanity_check",
+    "validate_yaml_config",
+]
 
 
 def sanity_check(yaml_file):
     """
     Check if the yaml file is properly indented, has valid mapping and tags.
     """
-    with open(yaml_file, 'r') as file:
+    with open(yaml_file, "r") as file:
         try:
             yaml_data = open_yaml_config(yaml_file)
         except yaml.parser.ParserError as e:
@@ -26,11 +30,13 @@ def sanity_check(yaml_file):
                 f"Incorrect mapping in the YAML_CONFIG file at line {e.problem_mark.line + 1}."
             )
         except yaml.constructor.ConstructorError as e:
-            print(f"Invalid tag in the YAML_CONFIG file at line {e.problem_mark.line + 1}.")
+            print(
+                f"Invalid tag in the YAML_CONFIG file at line {e.problem_mark.line + 1}."
+            )
         except yaml.reader.ReaderError as e:
             print(f"Failed to parse YAML file at line {e.problem_mark.line + 1}: {e}")
         except yaml.YAMLError as e:
-            if hasattr(e, 'problem_mark'):
+            if hasattr(e, "problem_mark"):
                 print(
                     f"Error in the YAML_CONFIG file at line {e.problem_mark.line}. "
                     "Please recheck the file."
@@ -66,3 +72,72 @@ def check_one_method_per_module(yaml_file):
             )
 
     return yaml_data
+
+
+def validate_yaml_config(yaml_file):
+    """
+    Check that the modules, methods, and parameters in the `YAML_CONFIG` file
+    are valid, and adhere to the same structure as in each corresponding
+    module in `httomo.templates`.
+    """
+    yaml_data = check_one_method_per_module(yaml_file)
+
+    modules = [next(iter(d)) for d in yaml_data]
+    methods = [next(iter(d.values())) for d in yaml_data]
+    packages = [m.split(".")[0] for m in modules]
+
+    #: the first method is always a loader
+    #: so `testing_pipeline.yaml` should not pass.
+    if next(iter(methods[0])) != "standard_tomo":
+        print(
+            "The first method in the YAML_CONFIG file must be a loader. "
+            "Please recheck the yaml file."
+        )
+        return False
+
+    parent_dir = os.path.dirname(os.path.abspath("__file__"))
+    templates_dir = os.path.join(parent_dir, "templates")
+    assert os.path.exists(templates_dir)
+
+    _template_yaml_files = [
+        os.path.join(
+            templates_dir, packages[i], modules[i], next(iter(methods[i])) + ".yaml"
+        )
+        for i in range(len(modules))
+    ]
+
+    for i, f in enumerate(_template_yaml_files):
+        if not os.path.exists(f):
+            print(
+                f"'{modules[i] + '/' + next(iter(methods[i]))}' is not a valid"
+                 " path to a method. Please recheck the yaml file."
+            )
+            return False
+
+    _template_yaml_data_list = [
+        next(iter(d.values()))
+        for f in _template_yaml_files
+        for d in open_yaml_config(f)
+    ]
+
+    for i, _ in enumerate(modules):
+        d1 = methods[i]
+        d2 = _template_yaml_data_list[i]
+
+        for key in d1.keys():
+            for parameter in d1[key].keys():
+                assert isinstance(parameter, str)
+                if parameter not in d2[key].keys():
+                    print(
+                        f"Parameter '{parameter}' in the '{modules[i]}' method is not valid."
+                    )
+                    return False
+
+                if None in (d1[key][parameter], d2[key][parameter]):
+                    continue
+
+                if not isinstance(d1[key][parameter], type(d2[key][parameter])):
+                    print(
+                        f"Value assigned to parameter '{parameter}' in the '{modules[i]}' method"
+                        f" is not correct. It should be of type {type(d2[key][parameter])}."
+                    )
