9e957ca82d194738dbd9ce94783688064a8eaf26
Team GPU <team-gpu@xcelerit.com>
2023-04-14 14:59:52 +0100

    Calculating and using platform sections

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -9,13 +9,13 @@ from httomo.wrappers_class import HttomolibWrapper
 def test_httomolib_wrapper_max_slices_cpu():
     wrp = HttomolibWrapper("misc", "images", "save_to_images", MPI.COMM_WORLD)
     assert wrp.cupyrun is False
-    assert wrp.calc_max_slices({}, 0, (100, 100), np.uint8, 50000) > 10000
+    assert wrp.calc_max_slices(0, (100, 100), np.uint8, 50000)[0] > 10000
 
 
 def test_httomolib_wrapper_max_slices_gpu():
     wrp = HttomolibWrapper("prep", "normalize", "normalize", MPI.COMM_WORLD)
     assert wrp.cupyrun is True
-    assert wrp.calc_max_slices({"flats": None}, 0, (100, 100), np.uint8, 50000) < 100000
+    assert wrp.calc_max_slices(0, (100, 100), np.uint8, 50000)[0] < 100000
 
 
 def test_httomolib_wrapper_max_slices_passes_kwargs():
@@ -25,10 +25,8 @@ def test_httomolib_wrapper_max_slices_passes_kwargs():
     mockMeta = dataclasses.replace(normalize.meta, calc_max_slices=mock_method)
     with mock.patch.object(normalize, "meta", mockMeta):
         wrp = HttomolibWrapper("prep", "normalize", "normalize", MPI.COMM_WORLD)
-
-        wrp.calc_max_slices(
-            dict(testarg=1, minus_log=True), 0, (100, 100), np.uint8(), 50000
-        )
+        wrp.dict_params = dict(testarg=1, minus_log=True)
+        wrp.calc_max_slices(0, (100, 100), np.uint8(), 50000)
 
     # make sure the default args are called and the args given above are overriding the defaults
     mock_method.assert_called_once_with(
