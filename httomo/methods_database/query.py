f5798c72ad0aa8ad1c97849ecfb6ea89766cbb83
Daniil Kazantsev <dkazanc@hotmail.com>
2023-04-20 16:20:38 +0100

    adding versions.yaml and make query to work with it

--- httomo/methods_database/query.py
+++ httomo/methods_database/query.py
@@ -31,9 +31,28 @@ def get_method_info(module_path: str, method_name: str, attr: str):
     """
     method_path = f"{module_path}.{method_name}"
     split_method_path = method_path.split(".")
-    package_name = split_method_path[0]
-    yaml_info_path = Path(YAML_DIR, f"{package_name}.yaml")
-
+    package_name = split_method_path[0]    
+    
+    # get information about the currently supported version of the package
+    yaml_versions_path = Path(YAML_DIR, "external/", "versions.yaml")
+    
+    if not yaml_versions_path.exists():
+        err_str = f"The YAML file {yaml_versions_path} doesn't exist."
+        raise ValueError(err_str)
+    
+    with open(yaml_versions_path, "r") as f:
+        yaml_versions_library = yaml.safe_load(f)
+    
+    ext_package_path = ""
+    for module, versions_dict in yaml_versions_library.items():
+        if module == package_name:
+            for version_type, package_version in versions_dict.items():
+                if version_type == "current":
+                    package_version = package_version[0]
+                    ext_package_path = f"external/{package_name}/{package_version}/"
+    
+    # open the library file for the package
+    yaml_info_path = Path(YAML_DIR, str(ext_package_path), f"{package_name}.yaml")
     if not yaml_info_path.exists():
         err_str = f"The YAML file {yaml_info_path} doesn't exist."
         log_exception(err_str)
