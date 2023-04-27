df3ccff1688df2fce755cfec58fe0ba5e32be956
Jessica V <jessica.verschoyle@diamond.ac.uk>
2023-04-27 11:12:11 +0100

    Rearrange to add a download all yaml link

--- docs/source/yaml_doc_generator.py
+++ docs/source/yaml_doc_generator.py
@@ -19,22 +19,25 @@
 import os
 
 
-def add_function_summary(doc_dir, name, files):
+def add_function_summary(doc_dir, root, files):
     """Append title and function summary to documentation file.
 
     Parameters
     ----------
     doc_dir : Path
         Documentation path
-    name : str
-        Module name.
+    root : Path
+        Path to the yaml template directory.
     files : List
-        List of functions for each module.
+        List of yaml function templates for each module.
     """
-    doc_rst_file = f"{doc_dir}/api/{name}.rst"
+    rst_name = root.split("/")[-1]
+    doc_rst_file = f"{doc_dir}/api/{rst_name}.rst"
     with open(doc_rst_file, "a") as edit_doc:
         if os.stat(doc_rst_file).st_size == 0:
+            rst_name = root.split("/")[-1]
             add_title(edit_doc, rst_name)
+            add_tomopy_link(edit_doc, rst_name)
         else:
             edit_doc.write(f"\n\n   .. rubric:: **Functions**")
             edit_doc.write(f"\n\n   .. autosummary::\n")
@@ -43,79 +46,113 @@ def add_function_summary(doc_dir, name, files):
                 edit_doc.write(f"\n       {yml_title}")
 
 
-def create_yaml_dropdown(doc_dir, name, files):
+def create_yaml_dropdown(doc_dir, root, files):
     """Create dropdown panels to allow yaml functions to be downloaded.
 
     Parameters
     ----------
     doc_dir : Path
         Documentation path
-    name : str
-        Module name.
+    root : Path
+        Path to the yaml template directory.
     files : List
         List of functions for each module.
     """
-    doc_rst_file = f"{doc_dir}/api/{name}.rst"
-    for fi in reversed(files):
-        t_name = root.split("source")[-1]
-        t_name = f"{t_name}/{fi}"
+    rst_name = root.split("/")[-1]
+    doc_rst_file = f"{doc_dir}/api/{rst_name}.rst"
+    template_dir = root.split("source")[-1]
+    download_all_button(template_dir, doc_rst_file)
+
+    for fi in files:
+        t_name = f"{template_dir}/{fi}"
         with open(doc_rst_file, "a") as edit_doc:
             edit_doc.write(f"\n\n.. dropdown:: {fi}")
             edit_doc.write(f"\n\n    :download:`Download <{t_name}>`")
             edit_doc.write(f"\n\n    .. literalinclude:: {t_name}")
 
 
+def download_all_button(template_dir, doc_rst_file):
+    """Download all yaml function text as one file.
+
+    Parameters
+    ----------
+    template_dir : str
+        Directory to template files.
+    doc_rst_file : File
+        rst file to write to.
+    """
+    download_all_path = f"{template_dir}/download_all.yaml"
+    # Only include download all when more than one yaml file exists
+    if len(files) > 1:
+        with open(doc_rst_file, "a") as edit_doc:
+            download_str = "\n\n:download:`Download all yaml templates<"
+            edit_doc.write(f"{download_str}{download_all_path}>`")
+
+
 def add_title(edit_doc, rst_name):
     """Add a title to rst file.
-    If it is a tomopy module, insert a link.
+    Parameters
+    ----------
+    edit_doc : File
+        Document to write to.
+    rst_name : str
+        name of rst file.
+    """
+    title = f":mod:`{rst_name}`"
+    edit_doc.write(f"{title}\n")
+    underline = len(title) * "="
+    edit_doc.write(f"{underline}\n")
+
+
+def add_tomopy_link(edit_doc, rst_name):
+    """ Link to tomopy documentation.
 
     Parameters
     ----------
     edit_doc : File
         Document to write to.
     rst_name : str
-        Module name.
+        name of rst file.
     """
-    title = f":mod:`{rst_name}`"
-    edit_doc.write(f"{title}\n")
-    underline = len(title) * "="
-    edit_doc.write(f"{underline}\n")
-    if "tomopy" in title:
+    if "tomopy" in rst_name:
+        # If it is a tomopy module, insert a link.
         url = "https://tomopy.readthedocs.io/en/stable/api/"
         edit_doc.write(f"\n{url}{rst_name}.html\n\n")
 
 
-def all_yaml(root, files):
-    """Create a file with all yaml function definitions.
+def save_all_yaml_functions(tmp_dir, yaml_files):
+    """Create a file including all yaml function definitions.
 
     Parameters
     ----------
-    root : Path
+    tmp_dir : Path
         Path to the yaml template directory.
-    files : list
-        List of individual yaml template method files.
+    yaml_files : list
+        List of individual yaml template files.
     """
-    filepath = f"{root}/download_all.yaml"
-    if len(files) > 1:
-        with open(filepath, 'w') as outfile:
-            for f in files:
-                with open(f"{root}/{f}") as infile:
+    file_path = f"{tmp_dir}/download_all.yaml"
+    # Only merge files when more than one yaml file exists
+    if len(yaml_files) > 1:
+        with open(file_path, 'w') as outfile:
+            for f in yaml_files:
+                with open(f"{tmp_dir}/{f}") as infile:
                     outfile.write(infile.read())
 
 
 if __name__ == "__main__":
-    # Create documentation for each module.
-    # Append yaml information to documentation pages.
+    """ Create documentation for modules from httomo, tomopy and httomolib.
+    Append the yaml information to the documentation pages.
+    """
     doc_source_dir = os.path.dirname(os.path.abspath(__file__))
     path_to_templates = doc_source_dir + '/../../templates/'
     for root, dirs, files in os.walk(path_to_templates, topdown=True):
         dirs[:] = [d for d in dirs]
         files[:] = [fi for fi in files if ".yaml" in fi]
+        # Don't include the module list on the documentation pages
         files[:] = [fi for fi in files if "modules" not in fi]
         if files:
-            rst_name = root.split("/")[-1]
-            add_function_summary(doc_source_dir, rst_name, files)
-            all_yaml(root, files)
-            create_yaml_dropdown(doc_source_dir, rst_name, files)
+            add_function_summary(doc_source_dir, root, files)
+            save_all_yaml_functions(root, files)
+            create_yaml_dropdown(doc_source_dir, root, files)
 
 
