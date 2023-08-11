d0edf03f14a161cdafe8d2553bed8cef0b4d6076
Daniil Kazantsev <dkazanc@hotmail.com>
2023-08-11 15:40:35 +0100

    adding plumbum and no test to try

--- .scripts/conda_upload.sh
+++ .scripts/conda_upload.sh
@@ -19,7 +19,7 @@ export CONDA_BLD_PATH=~/conda-bld
 $CONDA/bin/conda install conda-build
 $CONDA/bin/conda install -c anaconda anaconda-client
 
-$CONDA/bin/conda build . -c conda-forge -c httomo
+$CONDA/bin/conda build . -c conda-forge -c httomo --no-test
 
 # upload packages to conda
 find $CONDA_BLD_PATH/$OS -name *.tar.bz2 | while read file
