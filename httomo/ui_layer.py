d69840e0b8b31eb0daa869932659cb78e6686e22
algol <dkazanc@hotmail.com>
2023-12-12 16:38:52 +0000

    making the UI interface more generic, adding python scripting for tasks as a second option to yaml templates

--- httomo/ui_layer.py
+++ httomo/ui_layer.py
@@ -1,8 +1,9 @@
 import yaml
 from pathlib import Path
 from typing import Any, Dict, List, Protocol, TypeAlias
-from importlib import import_module
+from importlib import import_module, util
 import sys
+import os
 
 from mpi4py import MPI
 from mpi4py.MPI import Comm
@@ -17,36 +18,48 @@ from httomo.runner.pipeline import Pipeline
 from httomo.runner.backend_wrapper import make_backend_wrapper
 from httomo.runner.loader import make_loader
 from httomo.runner.output_ref import OutputRef
+from httomo.runner.task_runner import TaskRunner
 
 from datetime import datetime
-
 
 MethodConfig: TypeAlias = Dict[str, Dict[str, Any]]
 PipelineStageConfig: TypeAlias = List[MethodConfig]
 PipelineConfig: TypeAlias = List[PipelineStageConfig]
 
-class YamlInterface:
+class UiLayer:
+    """A common user interface for different front-ends in httomo.
+    We currently support YAML and Python interfaces, but in future
+    different UI's can be added based on other data formats, e.g. JSON. 
+    """
     
     def __init__(
         self,
-        yaml_file_tasks: str,
-        in_data_file: str,
+        tasks_file_path: str,
+        in_data_file_path: str,
         comm: Comm,
     ):   
         
         self.repo = MethodDatabaseRepository()
-        self.yaml_file_tasks = yaml_file_tasks
-        self.in_data_file = in_data_file        
+        self.tasks_file_path = tasks_file_path
+        self.in_data_file = in_data_file_path
         self.comm = comm
+        
+        root, ext = os.path.splitext(self.tasks_file_path)
+        if ext == '.yaml':
+            # loading yaml file with tasks provided
+            self.PipelineStageConfig = _yaml_loader(self.tasks_file_path)[0]
+        elif ext == '.py':
+            # loading python file with tasks provided
+            self.PipelineStageConfig = _python_tasks_loader(self.tasks_file_path)
+        else:
+            raise ValueError(
+                f"The extension {ext} of the file {root} with tasks is unknown."
+            )
 
-    def yaml_to_pipeline(self) -> Pipeline:
-        with open(self.yaml_file_tasks, "r") as f:
-            yaml_conf = list(yaml.load_all(f, Loader=yaml.FullLoader))
-        PipelineStageConfig = yaml_conf[0]
+    def build_pipeline(self) -> Pipeline:       
         output_mapping = {} # TODO
-
         methods_list = []
-        for task_conf in PipelineStageConfig:
+        for task_conf in self.PipelineStageConfig:
             if "loaders" in task_conf['module_path']:
                 task_conf['parameters']['in_file'] = self.in_data_file
                 # unpack params and initiate a loader
@@ -73,8 +86,20 @@ class YamlInterface:
         return Pipeline(
             loader=loader,
             methods=methods_list,
-            main_pipeline_start=3,
+            main_pipeline_start=1,
         )
+
+def _yaml_loader(file_path: str) -> list:
+    with open(file_path, "r") as f:
+        tasks_list = list(yaml.load_all(f, Loader=yaml.FullLoader))
+    return tasks_list
+
+def _python_tasks_loader(file_path: str) -> list:
+    module_spec = util.spec_from_file_location("methods_to_list", file_path)
+    foo = util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(foo)
+    tasks_list = list(foo.methods_to_list())
+    return tasks_list
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -92,12 +117,12 @@ if __name__ == "__main__":
         # Setup global logger object
         httomo.globals.logger = setup_logger(httomo.globals.run_out_dir)
 
-    from httomo.yaml_tools.yaml_to_pipeline import YamlInterface
+    from httomo.ui_layer import *
     
-    initialiseY = YamlInterface(sys.argv[1], sys.argv[2], comm=comm)
+    initYaml = UiLayer(sys.argv[1], sys.argv[2], comm=comm)
 
-    pipeline = initialiseY.yaml_to_pipeline()
+    pipeline = initYaml.build_pipeline()
 
-    print("done pipeline")
-    # runner = TaskRunner(pipeline, False)
-    #runner.execute()
+    #print("done pipeline")
+    runner = TaskRunner(pipeline, False)
+    runner.execute()
