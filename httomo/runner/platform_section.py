7cf91bbf0df6ffdf3657432aa06c58e64663cf5b
algol <dkazanc@hotmail.com>
2024-01-02 17:18:59 +0000

    test_tomo_stand passes

--- httomo/runner/platform_section.py
+++ httomo/runner/platform_section.py
@@ -48,10 +48,8 @@ def sectionize(pipeline: Pipeline, save_all: bool = False) -> List[PlatformSecti
             except:
                 pass
             return True
-        if save_all:
-            return True
         else:
-            return False
+            return save_all
 
     def needs_global_input(method: BackendWrapper) -> bool:
         return method.config_params.get("glob_stats", False)
