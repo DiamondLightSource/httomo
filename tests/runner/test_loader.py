bc3b4a671f6ef6ff1bfed2eabc1b7e02b1a22b72
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-01-24 14:22:14 +0000

    Add preview object and bound checking

--- tests/runner/test_loader.py
+++ tests/runner/test_loader.py
@@ -11,7 +11,7 @@ import numpy as np
 
 from httomo.data.hdf.loaders import LoaderData
 from httomo.runner.dataset import DataSet
-from httomo.runner.loader import DarksFlatsFileConfig, RawAngles, StandardTomoLoader, UserDefinedAngles, get_darks_flats
+from httomo.runner.loader import DarksFlatsFileConfig, Preview, PreviewConfig, PreviewDimConfig, RawAngles, StandardTomoLoader, UserDefinedAngles, get_darks_flats
 from ..testing_utils import make_mock_repo
 
 
@@ -383,6 +383,73 @@ def test_standard_tomo_loader_raises_error_slicing_dim(
             slicing_dim=SLICING_DIM,
             comm=COMM,
         )
+
+
+@pytest.mark.parametrize(
+    "preview_config, is_error_expected, err_str",
+    [
+        (
+            PreviewConfig(
+                angles=PreviewDimConfig(start=0, stop=221),
+                detector_y=PreviewDimConfig(start=0, stop=128),
+                detector_x=PreviewDimConfig(start=0, stop=160),
+            ),
+            True,
+            "Preview indices in angles dim exceed bounds of data: start=0, stop=221",
+        ),
+        (
+            PreviewConfig(
+                angles=PreviewDimConfig(start=0, stop=220),
+                detector_y=PreviewDimConfig(start=0, stop=129),
+                detector_x=PreviewDimConfig(start=0, stop=160),
+            ),
+            True,
+            "Preview indices in det y dim exceed bounds of data: start=0, stop=129",
+        ),
+        (
+            PreviewConfig(
+                angles=PreviewDimConfig(start=0, stop=220),
+                detector_y=PreviewDimConfig(start=0, stop=128),
+                detector_x=PreviewDimConfig(start=0, stop=161),
+            ),
+            True,
+            "Preview indices in det x dim exceed bounds of data: start=0, stop=161",
+        ),
+        (
+            PreviewConfig(
+                angles=PreviewDimConfig(start=0, stop=220),
+                detector_y=PreviewDimConfig(start=0, stop=128),
+                detector_x=PreviewDimConfig(start=0, stop=160),
+            ),
+            False,
+            "",
+        ),
+    ],
+    ids=[
+        "incorrect_angles_bounds",
+        "incorrect_det_y_bounds",
+        "incorrect_det_x_bounds",
+        "all_correct_bounds",
+    ],
+)
+def test_preview_bound_checking(
+    standard_data_path: str,
+    preview_config: PreviewConfig,
+    is_error_expected: bool,
+    err_str: str,
+):
+    IN_FILE = Path(__file__).parent.parent / "test_data/tomo_standard.nxs"
+    f = h5py.File(IN_FILE, "r")
+    dataset = f[standard_data_path]
+
+    if is_error_expected:
+        with pytest.raises(ValueError, match=err_str):
+            _ = Preview(preview_config, dataset)
+    else:
+        preview = Preview(preview_config, dataset)
+        assert preview.config == preview_config
+
+    f.close()
 
 
 def test_get_darks_flats_same_file_same_dataset(
