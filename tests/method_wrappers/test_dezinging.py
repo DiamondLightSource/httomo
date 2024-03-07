a30c6ba5da1fe1689f7ffcf6af65e347ead95e09
dkazanc <dkazanc@hotmail.com>
2024-03-07 13:23:31 +0000

    adds f/d testing in dezinger wrapper

--- tests/method_wrappers/test_dezinging.py
+++ tests/method_wrappers/test_dezinging.py
@@ -26,11 +26,16 @@ def test_dezinging(mocker: MockerFixture):
     assert isinstance(wrp, DezingingWrapper)
 
     GLOBAL_SHAPE = (10, 20, 30)
+    darks = 4 * np.ones((2, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32)
+    flats = 3 * np.ones((3, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32)
     aux_data = AuxiliaryData(
         angles=np.linspace(0, math.pi, GLOBAL_SHAPE[0], dtype=np.float32),
-        darks=4 * np.ones((2, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32),
-        flats=3 * np.ones((3, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32),
+        darks=darks,
+        flats=flats,
     )
+
+    darks_setter_spy = mocker.spy(aux_data, "set_darks")
+    flats_setter_spy = mocker.spy(aux_data, "set_flats")
     data = np.ones(GLOBAL_SHAPE, dtype=np.float32)
 
     block1 = DataSetBlock(
@@ -42,28 +47,7 @@ def test_dezinging(mocker: MockerFixture):
         chunk_shape=GLOBAL_SHAPE,
         global_shape=GLOBAL_SHAPE,
     )
-    spy = mocker.spy(wrp, "execute")
-    assert wrp._flats_darks_processed == False
 
-    newblock1 = wrp.execute(block1)
-    assert spy.call_count == 1
-    assert wrp._flats_darks_processed == True
-
-    # we double them all, so we expect all 3 to be twice the input
-    np.testing.assert_array_equal(newblock1.data, 2)
-    np.testing.assert_array_equal(newblock1.flats, 6)
-    np.testing.assert_array_equal(newblock1.darks, 8)
-
-    # It should only update the darks/flats once, so "_flats_darks_processed" should be True at this point.
-    # In order to ensure that we're not processing darks/flats again in the next block
-    # we change flats and darks array. If we DO, then the result must also change.
-    aux_data = AuxiliaryData(
-        angles=np.linspace(0, math.pi, GLOBAL_SHAPE[0], dtype=np.float32),
-        darks=8 * np.ones((2, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32),
-        flats=6 * np.ones((3, GLOBAL_SHAPE[1], GLOBAL_SHAPE[2]), dtype=np.float32),
-    )
-
-    # it should only update the darks/flats once, but still apply to the data
     block2 = DataSetBlock(
         data[2:4, :, :],
         aux_data=aux_data,
@@ -73,15 +57,34 @@ def test_dezinging(mocker: MockerFixture):
         chunk_shape=GLOBAL_SHAPE,
         global_shape=GLOBAL_SHAPE,
     )
+
+    # darks/flats setters on aux data object should be called when processing first block
+    assert wrp._flats_darks_processed is False
+    assert darks_setter_spy.call_count == 0
+    assert flats_setter_spy.call_count == 0
+    newblock1 = wrp.execute(block1)
+    assert wrp._flats_darks_processed is True
+    # it should only update the darks/flats once, but still apply to the data
+    assert darks_setter_spy.call_count == 1
+    assert flats_setter_spy.call_count == 1
+
+    # we double them all, so we expect all 3 to be twice the input
+    np.testing.assert_array_equal(newblock1.data, 2 * data[0:2, :, :])
+    np.testing.assert_array_equal(newblock1.flats, 2 * flats)
+    np.testing.assert_array_equal(newblock1.darks, 2 * darks)
+
     newblock2 = wrp.execute(block2)
-    np.testing.assert_array_equal(newblock2.data, 2)
-    np.testing.assert_array_equal(newblock2.flats, 6)
-    np.testing.assert_array_equal(newblock2.darks, 8)
+    np.testing.assert_array_equal(newblock2.data, 2 * data[2:4, :, :])
+    np.testing.assert_array_equal(newblock2.flats, 2 * flats)
+    np.testing.assert_array_equal(newblock2.darks, 2 * darks)
+    # it should only update the darks/flats once, but still apply to the data
+    assert darks_setter_spy.call_count == 1
+    assert flats_setter_spy.call_count == 1
 
     # the flats, darks in original dataset should have changed by reference
     aux_flats = aux_data.get_flats()
     aux_darks = aux_data.get_darks()
     assert aux_flats is not None
     assert aux_darks is not None
-    np.testing.assert_array_equal(aux_flats, 6)
-    np.testing.assert_array_equal(aux_darks, 8)
+    np.testing.assert_array_equal(aux_flats, 2 * flats)
+    np.testing.assert_array_equal(aux_darks, 2 * darks)
