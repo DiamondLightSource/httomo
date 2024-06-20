6374e12c08fc6f62690f127611862b7f06f40d82
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-06-20 16:08:40 +0100

    Add sink interface for param sweep result writing

--- httomo/sweep_runner/param_sweep_store_interfaces.py
+++ httomo/sweep_runner/param_sweep_store_interfaces.py
@@ -42,3 +42,45 @@ class ParamSweepSource(Protocol):
         each sweep result
         """
         ...  # pragma: no cover
+
+
+class ParamSweepSink(Protocol):
+    """Sink for writing parameter sweep results to"""
+
+    @property
+    def no_of_sweeps(self) -> int:
+        """Number of parameter values to sweep over"""
+        ...  # pragma: no cover
+
+    @property
+    def concat_dim(self) -> Literal[0, 1, 2]:
+        """Dimension to concatenate sweep results along - 0, 1, or 2"""
+        ...  # pragma: no cover
+
+    @property
+    def single_shape(self) -> Tuple[int, int, int]:
+        """Shape of single param sweep result"""
+        ...  # pragma: no cover
+
+    @property
+    def total_shape(self) -> Tuple[int, int, int]:
+        """
+        Shape of 3D array to contain all param sweep results concatenated along `concat_dim`
+        """
+        ...  # pragma: no cover
+
+    def write_sweep_result(self, block: DataSetBlock):
+        """
+        Writes single sweep result contained in a block to store along the `concat_dim` axis
+        """
+        ...  # pragma: no cover
+
+    def finalize(self):
+        """Method to be called after writing all blocks is done"""
+        ...  # pragma: no cover
+
+    def make_reader(self) -> ParamSweepSource:
+        """
+        Return a source from the sink, from which the data written to the sink can be read
+        """
+        ...  # pragma: no cover
