ac2ba1d2a9b2691e2a7559f4659a6e766b284f88
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-06-27 11:18:25 +0100

    Add method to sweep runner to load single block

--- httomo/sweep_runner/param_sweep_runner.py
+++ httomo/sweep_runner/param_sweep_runner.py
@@ -1,10 +1,16 @@
 from typing import Optional
 
+from httomo.runner.block_split import BlockSplitter
 from httomo.runner.dataset import DataSetBlock
+from httomo.runner.pipeline import Pipeline
 
 
 class ParamSweepRunner:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        pipeline: Pipeline,
+    ) -> None:
+        self._pipeline = pipeline
         self._block: Optional[DataSetBlock] = None
 
     @property
@@ -12,3 +18,11 @@ class ParamSweepRunner:
         if self._block is None:
             raise ValueError("Block from input data has not yet been loaded")
         return self._block
+
+    def prepare(self):
+        """
+        Load single block containing small number of sinogram slices in input data
+        """
+        source = self._pipeline.loader.make_data_source()
+        splitter = BlockSplitter(source, source.global_shape[source.slicing_dim])
+        self._block = splitter[0]
--- tests/sweep_runner/test_param_sweep_runner.py
+++ tests/sweep_runner/test_param_sweep_runner.py
@@ -1,10 +1,42 @@
 import pytest
+from pytest_mock import MockerFixture
 
+import numpy as np
+
+from httomo.runner.auxiliary_data import AuxiliaryData
+from httomo.runner.dataset import DataSetBlock
+from httomo.runner.pipeline import Pipeline
 from httomo.sweep_runner.param_sweep_runner import ParamSweepRunner
+from tests.testing_utils import make_test_loader
 
 
-def test_without_prepare_block_property_raises_error():
-    runner = ParamSweepRunner()
+def test_without_prepare_block_property_raises_error(mocker: MockerFixture):
+    loader = make_test_loader(mocker)
+    pipeline = Pipeline(loader=loader, methods=[])
+    runner = ParamSweepRunner(pipeline=pipeline)
     with pytest.raises(ValueError) as e:
         runner.block
     assert "Block from input data has not yet been loaded" in str(e)
+
+
+def test_after_prepare_block_attr_contains_data(mocker: MockerFixture):
+    GLOBAL_SHAPE = PREVIEWED_SLICES_SHAPE = (180, 3, 160)
+    data = np.arange(np.prod(PREVIEWED_SLICES_SHAPE), dtype=np.uint16).reshape(
+        PREVIEWED_SLICES_SHAPE
+    )
+    aux_data = AuxiliaryData(np.ones(PREVIEWED_SLICES_SHAPE[0], dtype=np.float32))
+    block = DataSetBlock(
+        data=data,
+        aux_data=aux_data,
+        slicing_dim=0,
+        global_shape=GLOBAL_SHAPE,
+        chunk_start=0,
+        chunk_shape=GLOBAL_SHAPE,
+        block_start=0,
+    )
+    loader = make_test_loader(mocker, block=block)
+    pipeline = Pipeline(loader=loader, methods=[])
+    runner = ParamSweepRunner(pipeline=pipeline)
+    runner.prepare()
+    assert runner.block is not None
+    np.testing.assert_array_equal(runner.block.data, data)
