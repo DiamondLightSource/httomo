34b0f947cb9e55a0ed0ac2b6933bcc85108930c3
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-08-13 15:23:23 +0100

    Account for previewing in padding before extrapolation function

--- httomo/data/padding.py
+++ httomo/data/padding.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 import h5py
 import numpy as np
+
+from httomo.preview import PreviewConfig
 
 
 def extrapolate_before(
@@ -8,6 +12,7 @@ def extrapolate_before(
     slices: int,
     dim: int,
     offset: int = 0,
+    preview_config: Optional[PreviewConfig] = None,
 ) -> None:
     """
     Read the required "before" padded area for a block into the given numpy array that
@@ -18,10 +23,25 @@ def extrapolate_before(
     """
     if slices == 0:
         return
+
+    DIMS = [0, 1, 2]
+    non_slicing_dims = tuple(set(DIMS) - set([dim]))
     slices_wrt = [slice(None), slice(None), slice(None)]
     slices_wrt[dim] = slice(slices)
+
     slices_read = [slice(None), slice(None), slice(None)]
-    slices_read[dim] = slice(offset, offset + 1)
+    if preview_config is None:
+        slices_read[dim] = slice(offset, offset + 1)
+    else:
+        slices_read[dim] = slice(
+            preview_config[dim].start, preview_config[dim].start + 1
+        )
+        for non_slicing_dim in non_slicing_dims:
+            slices_read[non_slicing_dim] = slice(
+                preview_config[non_slicing_dim].start,
+                preview_config[non_slicing_dim].stop,
+            )
+
     block_data[slices_wrt[0], slices_wrt[1], slices_wrt[2]] = global_data[
         slices_read[0], slices_read[1], slices_read[2]
     ]
--- tests/data/test_padding.py
+++ tests/data/test_padding.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 
 from httomo.data.padding import extrapolate_after, extrapolate_before
+from httomo.preview import PreviewConfig, PreviewDimConfig
 
 
 @pytest.mark.parametrize(
@@ -36,6 +37,84 @@ def test_extrapolate_before(slicing_dim: int):
     )
 
     extrapolate_before(global_data, block_data, BEFORE_PADDING, slicing_dim)
+    np.testing.assert_array_equal(block_data, expected_padded_block)
+
+
+@pytest.mark.parametrize(
+    "slicing_dim",
+    [0, 1],
+    ids=["projection-padding", "sinogram-padding"],
+)
+def test_extrapolate_before_previewed(slicing_dim: int):
+    GLOBAL_SHAPE = (180, 128, 160)
+    UNPADDED_BLOCK_LENGTH = 5
+    BEFORE_PADDING = 3
+    # Note the cropping in the `detector_y` dimension that shifts the start of the data from
+    # index 0 to index 10
+    PREVIEW_CONFIG = PreviewConfig(
+        angles=PreviewDimConfig(start=0, stop=180),
+        detector_y=PreviewDimConfig(start=10, stop=128),
+        detector_x=PreviewDimConfig(start=0, stop=160),
+    )
+    DIMS = [0, 1, 2]
+    non_slicing_dims = tuple(set(DIMS) - set([slicing_dim]))
+    global_data = np.arange(np.prod(GLOBAL_SHAPE), dtype=np.float32).reshape(
+        GLOBAL_SHAPE
+    )
+
+    # Due to the cropping being in the `detector_y` dimension:
+    # - if padding is in the `angles` dim, then the cropping affects the block shape's
+    # non-slicing dims
+    # - if padding is in the `detector_y` dim, then the cropping doesn't affect the block
+    # shape's non-slicing dims
+    padded_block_shape: List[int] = list(GLOBAL_SHAPE)
+    padded_block_shape[slicing_dim] = UNPADDED_BLOCK_LENGTH + BEFORE_PADDING
+    if slicing_dim == 0:
+        for dim in non_slicing_dims:
+            padded_block_shape[dim] = (
+                PREVIEW_CONFIG[dim].stop - PREVIEW_CONFIG[dim].start
+            )
+
+    # Setup numpy array containing the expected values after modification by
+    # `extrapolate_before()` (namely, that the "before" padded area has been filled in, and
+    # nothing else in the `expected_padded_block` array has been changed)
+    expected_padded_block = np.zeros(padded_block_shape, dtype=np.float32)
+
+    # If the slicing/padding dim is the `angles` dim, then the slice to repeat for the block's
+    # "before" padded area is the 0th slice in the `angles` dim, with `detector_y` cropped to
+    # start at index 10.
+    #
+    # If the slicing/padding dim is the `detector_y` dim, because the cropping is also in the
+    # `detector_y` dim, the padding needs to applied more carefully. The slice to be repeated
+    # for the "before" padded area needs to be the start of the cropped region along the
+    # `detector_y` dimension (ie, the slice with index 10).
+    slices_read = [slice(None)] * 3
+    slices_read[slicing_dim] = slice(
+        PREVIEW_CONFIG[slicing_dim].start,
+        PREVIEW_CONFIG[slicing_dim].start + 1,
+    )
+
+    if slicing_dim == 0:
+        for dim in non_slicing_dims:
+            slices_read[dim] = slice(
+                PREVIEW_CONFIG[dim].start, PREVIEW_CONFIG[dim].stop
+            )
+
+    slices_write = [slice(None)] * 3
+    slices_write[slicing_dim] = slice(BEFORE_PADDING)
+    expected_padded_block[slices_write[0], slices_write[1], slices_write[2]] = (
+        global_data[slices_read[0], slices_read[1], slices_read[2]]
+    )
+
+    # Block to have its "before" padded area filled in by the extrapolation function
+    block_data = np.zeros(padded_block_shape, dtype=np.float32)
+    extrapolate_before(
+        global_data,
+        block_data,
+        BEFORE_PADDING,
+        slicing_dim,
+        preview_config=PREVIEW_CONFIG,
+    )
     np.testing.assert_array_equal(block_data, expected_padded_block)
 
 
