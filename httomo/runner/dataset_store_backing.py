30114fff86ce3b7844437da7a562c2169b9bd382
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-09-09 15:51:23 +0100

    Add function to determine the store backing
    For any section that is not the last section, approximately two chunk
    copies need to be accounted for:
    - the unpadded chunk that the writer of section `n` creates
    - the padded chunk that the reader of section `n+1` creates
    
    For the last section in a pipeline, only one chunk copy needs to be
    accounted for: the unpadded chunk that the writer of that section
    creates.

--- httomo/runner/dataset_store_backing.py
+++ httomo/runner/dataset_store_backing.py
@@ -1,10 +1,11 @@
-from typing import Tuple
+from enum import Enum
+from typing import Callable, List, ParamSpec, Tuple
 
 import numpy as np
 from numpy.typing import DTypeLike
 from mpi4py import MPI
 
-from httomo.runner.section import Section
+from httomo.runner.section import Section, determine_section_padding
 from httomo.utils import _get_slicing_dim, make_3d_shape_from_shape
 
 
@@ -47,4 +48,131 @@ def calculate_section_chunk_bytes(
 
     return int(
         np.prod(non_slice_dims) * chunk_shape[slicing_dim] * np.dtype(dtype).itemsize
+    )
+
+
+class DataSetStoreBacking(Enum):
+    RAM = 1
+    File = 2
+
+
+P = ParamSpec("P")
+
+
+def _reduce_decorator_factory(
+    comm: MPI.Comm,
+) -> Callable[[Callable[P, DataSetStoreBacking]], Callable[P, DataSetStoreBacking]]:
+    """
+    Generate decorator for store-backing calculator function that will use the given MPI
+    communicator for the reduce operation.
+    """
+
+    def reduce_decorator(
+        func: Callable[P, DataSetStoreBacking]
+    ) -> Callable[P, DataSetStoreBacking]:
+        """
+        Decorator for store-backing calculator function.
+        """
+
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> DataSetStoreBacking:
+            """
+            Perform store-backing calculation across all MPI processes and reduce.
+            """
+            # reduce store backing enum variant across all processes - if any has
+            # `File` variant, all should use a file
+            send_buffer = np.zeros(1, dtype=bool)
+            recv_buffer = np.zeros(1, dtype=bool)
+            store_backing = func(*args, **kwargs)
+
+            if store_backing is DataSetStoreBacking.File:
+                send_buffer[0] = True
+
+            # do a logical or of all the enum variants across the processes
+            comm.Allreduce([send_buffer, MPI.BOOL], [recv_buffer, MPI.BOOL], MPI.LOR)
+
+            if bool(recv_buffer[0]) is True:
+                return DataSetStoreBacking.File
+
+            return DataSetStoreBacking.RAM
+
+        return wrapper
+
+    return reduce_decorator
+
+
+def _non_last_section_in_pipeline(
+    memory_limit_bytes: int,
+    write_chunk_bytes: int,
+    read_chunk_bytes: int,
+) -> DataSetStoreBacking:
+    """
+    Calculate backing of dataset store for non-last sections in pipeline
+    """
+    if (
+        memory_limit_bytes > 0
+        and write_chunk_bytes + read_chunk_bytes >= memory_limit_bytes
+    ):
+        return DataSetStoreBacking.File
+
+    return DataSetStoreBacking.RAM
+
+
+def _last_section_in_pipeline(
+    memory_limit_bytes: int,
+    write_chunk_bytes: int,
+) -> DataSetStoreBacking:
+    """
+    Calculate backing of dataset store for last section in pipeline
+    """
+    if memory_limit_bytes > 0 and write_chunk_bytes >= memory_limit_bytes:
+        return DataSetStoreBacking.File
+
+    return DataSetStoreBacking.RAM
+
+
+def determine_store_backing(
+    comm: MPI.Comm,
+    sections: List[Section],
+    memory_limit_bytes: int,
+    dtype: DTypeLike,
+    global_shape: Tuple[int, int, int],
+    section_idx: int,
+) -> DataSetStoreBacking:
+    reduce_decorator = _reduce_decorator_factory(comm)
+
+    # Get chunk shape input to section
+    current_chunk_shape = calculate_section_chunk_shape(
+        comm=comm,
+        global_shape=global_shape,
+        slicing_dim=_get_slicing_dim(sections[section_idx].pattern) - 1,
+        padding=(0, 0),
+    )
+
+    # Get the number of bytes in the input chunk to the section w/ potential modifications to
+    # the non-slicing dims
+    current_chunk_bytes = calculate_section_chunk_bytes(
+        chunk_shape=current_chunk_shape,
+        dtype=dtype,
+        section=sections[section_idx],
+    )
+
+    if section_idx == len(sections) - 1:
+        return reduce_decorator(_last_section_in_pipeline)(
+            memory_limit_bytes=memory_limit_bytes,
+            write_chunk_bytes=current_chunk_bytes,
+        )
+
+    # Get chunk shape created by reader of section `n+1`, that will add padding to the
+    # chunk shape written by the writer of section `n`
+    next_chunk_shape = calculate_section_chunk_shape(
+        comm=comm,
+        global_shape=global_shape,
+        slicing_dim=_get_slicing_dim(sections[section_idx + 1].pattern) - 1,
+        padding=determine_section_padding(sections[section_idx + 1]),
+    )
+    next_chunk_bytes = int(np.prod(next_chunk_shape) * np.dtype(dtype).itemsize)
+    return reduce_decorator(_non_last_section_in_pipeline)(
+        memory_limit_bytes=memory_limit_bytes,
+        write_chunk_bytes=current_chunk_bytes,
+        read_chunk_bytes=next_chunk_bytes,
     )
--- tests/runner/test_dataset_store_backing.py
+++ tests/runner/test_dataset_store_backing.py
@@ -6,8 +6,10 @@ from mpi4py import MPI
 from pytest_mock import MockerFixture
 
 from httomo.runner.dataset_store_backing import (
+    DataSetStoreBacking,
     calculate_section_chunk_shape,
     calculate_section_chunk_bytes,
+    determine_store_backing,
 )
 from httomo.runner.methods_repository_interface import GpuMemoryRequirement
 from httomo.runner.pipeline import Pipeline
@@ -173,3 +175,341 @@ def test_calculate_section_chunk_bytes_output_dims_change_and_swap(
         section=sections[0],
     )
     assert section_output_chunk_bytes == EXPECTED_SECTION_OUTPUT_CHUNK_BYTES
+
+
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (3 * 1024**2, DataSetStoreBacking.File),
+        (4 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["3MB-limit-file-backing", "4MB-limit-ram-backing"],
+)
+def test_determine_store_backing_last_section_pipeline_single_proc(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For a single process, chunk shape = global shape
+    #
+    # The dtype and shape combined makes:
+    # - the write chunk ~3.4MB
+    # - the read chunk also ~3.4MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    method = make_test_method(
+        mocker=mocker, method_name="method", pattern=Pattern.projection
+    )
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[method],
+    )
+    sections = sectionize(pipeline)
+
+    # Based on memory limit and the given section in the pipeline, determine the backing of the
+    # store for the execution of that section
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
+
+
+@pytest.mark.mpi
+@pytest.mark.skipif(
+    MPI.COMM_WORLD.size != 2, reason="Only rank-2 MPI is supported with this test"
+)
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (1 * 1024**2, DataSetStoreBacking.File),
+        (2 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["1MB-limit-file-backing", "2MB-limit-ram-backing"],
+)
+def test_determine_store_backing_last_section_pipeline_two_procs(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For two processes, chunk shape = half of global shape
+    #
+    # The dtype and shape combined makes:
+    # - the write chunk ~1.7MB
+    # - the read chunk also ~1.7MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    method = make_test_method(
+        mocker=mocker, method_name="method", pattern=Pattern.projection
+    )
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[method],
+    )
+    sections = sectionize(pipeline)
+
+    # Based on memory limit and the given section in the pipeline, determine the backing of the
+    # store for the execution of that section
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
+
+
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (6 * 1024**2, DataSetStoreBacking.File),
+        (7 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["6MB-limit-file-backing", "7MB-limit-ram-backing"],
+)
+def test_determine_store_backing_non_last_section_pipeline_single_proc(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For a single process, chunk shape = global shape
+    #
+    # The dtype and shape combined makes:
+    # - the write chunk ~3.4MB
+    # - the read chunk also ~3.4MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(mocker=mocker, method_name="m1", pattern=Pattern.projection)
+    m2 = make_test_method(mocker=mocker, method_name="m2", pattern=Pattern.sinogram)
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[m1, m2],
+    )
+    sections = sectionize(pipeline)
+
+    # For execution of non-last sections in pipelines, the writer must take into account that a
+    # copy of the chunk is made by the reader of the following section. Therefore, two copies
+    # of the chunk must be taken into account when deciding the backing of the store.
+    #
+    # Note that section 0 is only the section that is "not the last section", so it's the only
+    # one that will need to account for two copies of the chunk, and thus the main target of
+    # the test. Hence, why `section_idx=0` is given.
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
+
+
+@pytest.mark.mpi
+@pytest.mark.skipif(
+    MPI.COMM_WORLD.size != 2, reason="Only rank-2 MPI is supported with this test"
+)
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (3 * 1024**2, DataSetStoreBacking.File),
+        (4 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["3MB-limit-file-backing", "4MB-limit-ram-backing"],
+)
+def test_determine_store_backing_non_last_section_pipeline_two_procs(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For two processes, chunk shape = half of global shape
+    #
+    # The dtype and shape combined makes:
+    # - the write chunk ~1.7MB
+    # - the read chunk also ~1.7MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(mocker=mocker, method_name="m1", pattern=Pattern.projection)
+    m2 = make_test_method(mocker=mocker, method_name="m2", pattern=Pattern.sinogram)
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[m1, m2],
+    )
+    sections = sectionize(pipeline)
+
+    # For exeuction of non-last sections in pipelines, the writer must take into account that a
+    # copy of the chunk is made by the reader of the following section. Therefore, two copies
+    # of the chunk must be taken into account when deciding the backing of the store.
+    #
+    # Note that section 0 is only the section that is "not the last section", so it's the only
+    # one that will need to account for two copies of the chunk, and thus the main target of
+    # the test. Hence, why `section_idx=0` is given.
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
+
+
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (7 * 1024**2, DataSetStoreBacking.File),
+        (10 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["7MB-limit-file-backing", "10MB-limit-ram-backing"],
+)
+def test_determine_store_backing_non_last_section_pipeline_large_padding_single_proc(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For a single process, chunk shape = global shape
+    #
+    # The dtype, shape, and padding combined makes:
+    # - the write chunk ~3.4MB
+    # - the read chunk ~5.7MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+    PADDING = (50, 50)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(mocker=mocker, method_name="m1", pattern=Pattern.projection)
+    m2 = make_test_method(
+        mocker=mocker, method_name="m2", pattern=Pattern.sinogram, padding=True
+    )
+    mocker.patch.object(
+        target=m2,
+        attribute="calculate_padding",
+        return_value=PADDING,
+    )
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[m1, m2],
+    )
+    sections = sectionize(pipeline)
+
+    # For execution of non-last sections in pipelines, the writer must take into account that a
+    # copy of the chunk is made by the reader of the following section. Therefore, two copies
+    # of the chunk must be taken into account when deciding the backing of the store.
+    #
+    # Note that section 0 is only the section that is "not the last section", so it's the only
+    # one that will need to account for two copies of the chunk, and thus the main target of
+    # the test. Hence, why `section_idx=0` is given.
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
+
+
+@pytest.mark.mpi
+@pytest.mark.skipif(
+    MPI.COMM_WORLD.size != 2, reason="Only rank-2 MPI is supported with this test"
+)
+@pytest.mark.parametrize(
+    "memory_limit, expected_store_backing",
+    [
+        (4 * 1024**2, DataSetStoreBacking.File),
+        (5 * 1024**2, DataSetStoreBacking.RAM),
+    ],
+    ids=["4MB-limit-file-backing", "5MB-limit-ram-backing"],
+)
+def test_determine_store_backing_non_last_section_pipeline_large_padding_two_procs(
+    mocker: MockerFixture,
+    memory_limit: int,
+    expected_store_backing: DataSetStoreBacking,
+):
+    COMM = MPI.COMM_WORLD
+
+    # For a single process, chunk shape = global shape
+    #
+    # The dtype, shape, and padding combined makes:
+    # - the write chunk ~1.7MB
+    # - the read chunk ~2.8MB
+    DTYPE = np.float32
+    GLOBAL_SHAPE = (10, 300, 300)
+    PADDING = (50, 50)
+
+    # Define dummy loader and method wrapper objects
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(mocker=mocker, method_name="m1", pattern=Pattern.projection)
+    m2 = make_test_method(
+        mocker=mocker, method_name="m2", pattern=Pattern.sinogram, padding=True
+    )
+    mocker.patch.object(
+        target=m2,
+        attribute="calculate_padding",
+        return_value=PADDING,
+    )
+
+    # Get list of section objects that represent pipeline
+    pipeline = Pipeline(
+        loader=loader,
+        methods=[m1, m2],
+    )
+    sections = sectionize(pipeline)
+
+    # For execution of non-last sections in pipelines, the writer must take into account that a
+    # copy of the chunk is made by the reader of the following section. Therefore, two copies
+    # of the chunk must be taken into account when deciding the backing of the store.
+    #
+    # Note that section 0 is only the section that is "not the last section", so it's the only
+    # one that will need to account for two copies of the chunk, and thus the main target of
+    # the test. Hence, why `section_idx=0` is given.
+    store_backing = determine_store_backing(
+        comm=COMM,
+        sections=sections,
+        memory_limit_bytes=memory_limit,
+        dtype=DTYPE,
+        global_shape=GLOBAL_SHAPE,
+        section_idx=0,
+    )
+    assert store_backing is expected_store_backing
