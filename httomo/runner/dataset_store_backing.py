1429838ee40ba9f5cf5ac775156e0de0b4df716d
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-09-09 11:14:44 +0100

    Add section output chunk bytes calculator

--- httomo/runner/dataset_store_backing.py
+++ httomo/runner/dataset_store_backing.py
@@ -1,8 +1,11 @@
 from typing import Tuple
 
+import numpy as np
+from numpy.typing import DTypeLike
 from mpi4py import MPI
 
-from httomo.utils import make_3d_shape_from_shape
+from httomo.runner.section import Section
+from httomo.utils import _get_slicing_dim, make_3d_shape_from_shape
 
 
 def calculate_section_chunk_shape(
@@ -20,3 +23,28 @@ def calculate_section_chunk_shape(
     shape = list(global_shape)
     shape[slicing_dim] = section_slicing_dim_len + padding[0] + padding[1]
     return make_3d_shape_from_shape(shape)
+
+
+def calculate_section_chunk_bytes(
+    chunk_shape: Tuple[int, int, int],
+    dtype: DTypeLike,
+    section: Section,
+) -> int:
+    """
+    Calculate the number of bytes in the section output chunk that is written to the store. Ths
+    accounts for data's non-slicing dims changing during processing, which changes the chunk
+    shape for the section and thus affects the number of bytes in the chunk.
+    """
+    slicing_dim = _get_slicing_dim(section.pattern) - 1
+    non_slice_dims_list = list(chunk_shape)
+    non_slice_dims_list.pop(slicing_dim)
+    non_slice_dims = (non_slice_dims_list[0], non_slice_dims_list[1])
+
+    for method in section.methods:
+        if method.memory_gpu is None:
+            continue
+        non_slice_dims = method.calculate_output_dims(non_slice_dims)
+
+    return int(
+        np.prod(non_slice_dims) * chunk_shape[slicing_dim] * np.dtype(dtype).itemsize
+    )
--- tests/runner/test_dataset_store_backing.py
+++ tests/runner/test_dataset_store_backing.py
@@ -1,11 +1,19 @@
 from typing import List, Tuple
 
 import pytest
+import numpy as np
 from mpi4py import MPI
 from pytest_mock import MockerFixture
 
-from httomo.runner.dataset_store_backing import calculate_section_chunk_shape
-from httomo.utils import make_3d_shape_from_shape
+from httomo.runner.dataset_store_backing import (
+    calculate_section_chunk_shape,
+    calculate_section_chunk_bytes,
+)
+from httomo.runner.methods_repository_interface import GpuMemoryRequirement
+from httomo.runner.pipeline import Pipeline
+from httomo.runner.section import sectionize
+from httomo.utils import Pattern, make_3d_shape_from_shape
+from tests.testing_utils import make_test_loader, make_test_method
 
 
 @pytest.mark.parametrize(
@@ -59,3 +67,109 @@ def test_calculate_section_chunk_shape(
         padding=section_padding,
     )
     assert section_chunk_shape == make_3d_shape_from_shape(expected_chunk_shape)
+
+
+def test_calculate_section_chunk_bytes_output_dims_change(mocker: MockerFixture):
+    NEW_OUTPUT_DIMS = (300, 400)
+    SECTION_INPUT_CHUNK_SHAPE = (100, 10, 100)
+    DTYPE = np.float32
+    EXPECTED_SECTION_OUTPUT_CHUNK_SHAPE = (
+        SECTION_INPUT_CHUNK_SHAPE[0],
+        NEW_OUTPUT_DIMS[0],
+        NEW_OUTPUT_DIMS[1],
+    )
+    EXPECTED_SECTION_OUTPUT_CHUNK_BYTES = (
+        np.prod(EXPECTED_SECTION_OUTPUT_CHUNK_SHAPE) * np.dtype(DTYPE).itemsize
+    )
+
+    # Define methods to form section, one of which changes the output shape
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(mocker=mocker, method_name="m1", pattern=Pattern.projection)
+    m2 = make_test_method(
+        mocker=mocker, method_name="m2", pattern=Pattern.projection, gpu=True
+    )
+    mocker.patch.object(
+        m2,
+        "memory_gpu",
+        [GpuMemoryRequirement(multiplier=2.0, method="direct")],
+    )
+    mocker.patch.object(
+        target=m2,
+        attribute="calculate_output_dims",
+        return_value=NEW_OUTPUT_DIMS,
+    )
+
+    # Generate list of sections
+    pipeline = Pipeline(loader=loader, methods=[m1, m2])
+    sections = sectionize(pipeline)
+
+    # Check that the number of bytes in the chunk accounts for the non-slicing dims change by
+    # the method in the section
+    section_output_chunk_bytes = calculate_section_chunk_bytes(
+        chunk_shape=SECTION_INPUT_CHUNK_SHAPE,
+        dtype=DTYPE,
+        section=sections[0],
+    )
+    assert section_output_chunk_bytes == EXPECTED_SECTION_OUTPUT_CHUNK_BYTES
+
+
+def test_calculate_section_chunk_bytes_output_dims_change_and_swap(
+    mocker: MockerFixture,
+):
+    RECON_SIZE = 400
+    SECTION_INPUT_CHUNK_SHAPE = (100, 10, 100)
+    DTYPE = np.float32
+    EXPECTED_SECTION_OUTPUT_CHUNK_SHAPE = (
+        SECTION_INPUT_CHUNK_SHAPE[1],
+        RECON_SIZE,
+        RECON_SIZE,
+    )
+    EXPECTED_SECTION_OUTPUT_CHUNK_BYTES = (
+        np.prod(EXPECTED_SECTION_OUTPUT_CHUNK_SHAPE) * np.dtype(DTYPE).itemsize
+    )
+
+    # Define methods to form section, one of which changes the output shape and also swaps the
+    # output dims 0 and 1 (which can happen in recon methods)
+    loader = make_test_loader(mocker=mocker)
+    m1 = make_test_method(
+        mocker=mocker, method_name="stripe-removal", pattern=Pattern.sinogram
+    )
+    # NOTE: The reason that `swap_dims_on_output=True` is not passed to `make_test_method()`
+    # even though `m2` is assumed to be swapping the output dims is because that doesn't
+    # actually do anything when not running the method on data.
+    #
+    # The output dims being swapped is not taken into account when calling
+    # `calculate_output_dims()` on a method wrapper object, and the output dims swapping is
+    # implicitly done when the method executes.
+    #
+    # It however still seemed reasonable to check that the correct chunk bytes value was given
+    # if there's a method in a section that swaps the output dims.
+    m2 = make_test_method(
+        mocker=mocker,
+        method_name="recon",
+        pattern=Pattern.sinogram,
+        gpu=True,
+    )
+    mocker.patch.object(
+        m2,
+        "memory_gpu",
+        [GpuMemoryRequirement(multiplier=2.0, method="direct")],
+    )
+    mocker.patch.object(
+        target=m2,
+        attribute="calculate_output_dims",
+        return_value=(RECON_SIZE, RECON_SIZE),
+    )
+
+    # Generate list of sections
+    pipeline = Pipeline(loader=loader, methods=[m1, m2])
+    sections = sectionize(pipeline)
+
+    # Check that the number of bytes in the chunk accounts for the non-slicing dims change by
+    # the method in the section
+    section_output_chunk_bytes = calculate_section_chunk_bytes(
+        chunk_shape=SECTION_INPUT_CHUNK_SHAPE,
+        dtype=DTYPE,
+        section=sections[0],
+    )
+    assert section_output_chunk_bytes == EXPECTED_SECTION_OUTPUT_CHUNK_BYTES
