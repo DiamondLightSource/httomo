79e4b77ee25e4265f1422c9468924f5cf8772fdd
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-10-16 11:11:35 +0100

    Use `Union` in `generic_array` type alias
    See #492 for more details

--- httomo/types.py
+++ httomo/types.py
@@ -1,8 +1,8 @@
-from typing import TypeAlias
+from typing import TypeAlias, Union
 
 import numpy as np
 
 from httomo.utils import xp
 
 
-generic_array: TypeAlias = np.ndarray | xp.ndarray
+generic_array: TypeAlias = Union[np.ndarray, xp.ndarray]
