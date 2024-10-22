53c636909af3505f62bd7d38aae0f32a269685e8
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2024-10-22 13:00:06 +0100

    Select distortion correction wrapper for distortion correction methods only

---
+++ httomo/method_wrappers/distortion_correction.py
@@ -0,0 +1,36 @@
+from typing import Dict, Optional
+
+from mpi4py.MPI import Comm
+
+from httomo.method_wrappers.generic import GenericMethodWrapper
+from httomo.runner.methods_repository_interface import MethodRepository
+
+
+class DistortionCorrectionWrapper(GenericMethodWrapper):
+    """
+    Wrapper for distortion correction methods.
+    """
+
+    @classmethod
+    def should_select_this_class(cls, module_path: str, method_name: str) -> bool:
+        return "distortion_correction" in method_name
+
+    def __init__(
+        self,
+        method_repository: MethodRepository,
+        module_path: str,
+        method_name: str,
+        comm: Comm,
+        save_result: Optional[bool] = None,
+        output_mapping: Dict[str, str] = {},
+        **kwargs,
+    ):
+        super().__init__(
+            method_repository,
+            module_path,
+            method_name,
+            comm,
+            save_result,
+            output_mapping,
+            **kwargs,
+        )
---
+++ tests/method_wrappers/test_distortion_correction.py
@@ -0,0 +1,19 @@
+import pytest
+
+from httomo.method_wrappers.distortion_correction import DistortionCorrectionWrapper
+
+
+@pytest.mark.parametrize(
+    "method_name, expected_result",
+    [("distortion_correction", True), ("other_method", False)],
+    ids=["should-select", "shouldn't-select"],
+)
+def test_class_only_selected_for_methods_with_distortion_correction_in_name(
+    method_name: str, expected_result: bool
+):
+    assert (
+        DistortionCorrectionWrapper.should_select_this_class(
+            "dummy.module.path", method_name
+        )
+        is expected_result
+    )
