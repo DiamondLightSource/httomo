93e4698faf4e13b4be129895a5af97afe0a80251
dkazanc <dkazanc@hotmail.com>
2025-02-17 14:54:40 +0000

    tests modification

--- tests/test_pipeline_big.py
+++ tests/test_pipeline_big.py
@@ -217,60 +217,60 @@ def test_pipeline_gpu_FBP_diad_k11_38730_in_memory(
 
 
 # TODO one needs to add the test of the result of the denoising method here
-@pytest.mark.full_data
-def test_pipeline_gpu_FBP_denoising_i13_177906_in_disk(
-    get_files: Callable,
-    cmd,
-    i13_177906,
-    gpu_pipelineFBP_denoising,
-    gpu_diad_FBP_k11_38731_npz,  # TODO change this
-    output_folder,
-):
-    # NOTE that the intermediate file with file-based processing will be saved to /tmp
+# @pytest.mark.full_data
+# def test_pipeline_gpu_FBP_denoising_i13_177906_in_disk(
+#     get_files: Callable,
+#     cmd,
+#     i13_177906,
+#     gpu_pipelineFBP_denoising,
+#     gpu_diad_FBP_k11_38731_npz,  # TODO change this
+#     output_folder,
+# ):
+#     # NOTE that the intermediate file with file-based processing will be saved to /tmp
 
-    cmd.pop(4)  #: don't save all
-    cmd.insert(5, i13_177906)
-    cmd.insert(7, gpu_pipelineFBP_denoising)
-    cmd.insert(8, output_folder)
-    cmd.insert(9, "--max-memory")
-    cmd.insert(10, "40G")
-    cmd.insert(11, "--reslice-dir")
-    cmd.insert(12, "/scratch/jenkins_agent/workspace/")
+#     cmd.pop(4)  #: don't save all
+#     cmd.insert(5, i13_177906)
+#     cmd.insert(7, gpu_pipelineFBP_denoising)
+#     cmd.insert(8, output_folder)
+#     cmd.insert(9, "--max-memory")
+#     cmd.insert(10, "40G")
+#     cmd.insert(11, "--reslice-dir")
+#     cmd.insert(12, "/scratch/jenkins_agent/workspace/")
 
-    subprocess.check_output(cmd)
+#     subprocess.check_output(cmd)
 
-    files = get_files("output_dir/")
+#     files = get_files("output_dir/")
 
-    #: check the generated reconstruction (hdf5 file)
-    h5_files = list(filter(lambda x: ".h5" in x, files))
-    assert len(h5_files) == 1
+#     #: check the generated reconstruction (hdf5 file)
+#     h5_files = list(filter(lambda x: ".h5" in x, files))
+#     assert len(h5_files) == 1
 
-    # load the pre-saved numpy array for comparison bellow
-    data_gt = gpu_diad_FBP_k11_38731_npz["data"]  # TODO change this
-    axis_slice = gpu_diad_FBP_k11_38731_npz["axis_slice"]  # TODO change this
-    (slices, sizeX, sizeY) = np.shape(data_gt)
+#     # load the pre-saved numpy array for comparison bellow
+#     data_gt = gpu_diad_FBP_k11_38731_npz["data"]  # TODO change this
+#     axis_slice = gpu_diad_FBP_k11_38731_npz["axis_slice"]  # TODO change this
+#     (slices, sizeX, sizeY) = np.shape(data_gt)
 
-    step = axis_slice // (slices + 2)
-    # store for the result
-    data_result = np.zeros((slices, sizeX, sizeY), dtype=np.float32)
+#     step = axis_slice // (slices + 2)
+#     # store for the result
+#     data_result = np.zeros((slices, sizeX, sizeY), dtype=np.float32)
 
-    # TODO re-enable this
+# TODO re-enable this
 
-    # path_to_data = "data/"
-    # for file_to_open in h5_files:
-    #     if "httomolibgpu-FBP" in file_to_open:
-    #         h5f = h5py.File(file_to_open, "r")
-    #         index_prog = step
-    #         for i in range(slices):
-    #             data_result[i, :, :] = h5f[path_to_data][:, index_prog, :]
-    #             index_prog += step
-    #         h5f.close()
-    #     else:
-    #         raise FileNotFoundError("File with httomolibgpu-FBP string cannot be found")
+# path_to_data = "data/"
+# for file_to_open in h5_files:
+#     if "httomolibgpu-FBP" in file_to_open:
+#         h5f = h5py.File(file_to_open, "r")
+#         index_prog = step
+#         for i in range(slices):
+#             data_result[i, :, :] = h5f[path_to_data][:, index_prog, :]
+#             index_prog += step
+#         h5f.close()
+#     else:
+#         raise FileNotFoundError("File with httomolibgpu-FBP string cannot be found")
 
-    # residual_im = data_gt - data_result
-    # res_norm = np.linalg.norm(residual_im.flatten()).astype("float32")
-    # assert res_norm < 1e-6
+# residual_im = data_gt - data_result
+# res_norm = np.linalg.norm(residual_im.flatten()).astype("float32")
+# assert res_norm < 1e-6
 
 
 @pytest.mark.full_data
@@ -282,6 +282,22 @@ def test_pipeline_gpu_FBP_denoising_i13_177906_in_memory(
     gpu_diad_FBP_k11_38731_npz,  # TODO change this
     output_folder,
 ):
+
+    _change_value_parameters_method_pipeline(
+        gpu_pipelineFBP_denoising,
+        method=[
+            "total_variation_PD",
+            "total_variation_PD",
+        ],
+        key=[
+            "regularisation_parameter",
+            "iterations",
+        ],
+        value=[
+            1.0e-04,
+            50,
+        ],
+    )
 
     cmd.pop(4)  #: don't save all
     cmd.insert(5, i13_177906)
