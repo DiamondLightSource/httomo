a5ae089bde4b5d90511fd1f9d313d09cb233e02b
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2025-03-06 12:54:01 +0000

    Remove unused compressed tomo standard test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -134,15 +134,6 @@ def test_data_path():
     return os.path.join(CUR_DIR, "test_data")
 
 
-# only load from disk once per session, and we use np.copy for the elements,
-# to ensure data in this loaded file stays as originally loaded
-@pytest.fixture(scope="session")
-def data_file(test_data_path):
-    in_file = os.path.join(test_data_path, "tomo_standard.npz")
-    # keys: data, flats, darks, angles, angles_total, detector_y, detector_x
-    return np.load(in_file)
-
-
 @pytest.fixture
 def ensure_clean_memory():
     import cupy as cp
@@ -156,54 +147,6 @@ def ensure_clean_memory():
     cp.get_default_pinned_memory_pool().free_all_blocks()
     cache = cp.fft.config.get_plan_cache()
     cache.clear()
-
-
-@pytest.fixture
-def host_data(data_file):
-    return np.float32(np.copy(data_file["data"]))
-
-
-@pytest.fixture
-def data(host_data, ensure_clean_memory):
-    import cupy as cp
-
-    return cp.asarray(host_data)
-
-
-@pytest.fixture
-def host_angles(data_file):
-    return np.float32(np.copy(data_file["angles"]))
-
-
-@pytest.fixture
-def host_angles_radians(host_angles):
-    return host_angles
-
-
-@pytest.fixture
-def host_flats(data_file):
-    return np.float32(np.copy(data_file["flats"]))
-
-
-@pytest.fixture
-def flats(host_flats, ensure_clean_memory):
-    import cupy as cp
-
-    return cp.asarray(host_flats)
-
-
-@pytest.fixture
-def host_darks(
-    data_file,
-):
-    return np.float32(np.copy(data_file["darks"]))
-
-
-@pytest.fixture
-def darks(host_darks, ensure_clean_memory):
-    import cupy as cp
-
-    return cp.asarray(host_darks)
 
 
 @pytest.fixture
