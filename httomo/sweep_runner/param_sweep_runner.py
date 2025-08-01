fb39aee6f4b01ac4def89029e20b4cb301c4d681
dkazanc <dkazanc@hotmail.com>
2025-08-01 09:49:52 +0100

    removes the unused code

--- httomo/sweep_runner/param_sweep_runner.py
+++ httomo/sweep_runner/param_sweep_runner.py
@@ -46,7 +46,6 @@ class ParamSweepRunner:
             self._start_sweep_idx : self._stop_sweep_idx
         ]
         self._set_image_saver_params()
-        # self._set_rescale_to_int_params()
 
     @property
     def block(self) -> ParamSweepBlock:
@@ -97,23 +96,6 @@ class ParamSweepRunner:
                 if isinstance(method, ImagesWrapper):
                     method["offset"] = self._start_sweep_idx
                     method["watermark_vals"] = self._sweep_values
-
-    # def _set_rescale_to_int_params(self) -> None:
-    #     """ """
-    #     non_sweep_stages = [self._stages.before_sweep, self._stages.after_sweep]
-    #     for non_sweep_stage in non_sweep_stages:
-    #         for method in non_sweep_stage.methods:
-    #             if method.method_name == "rescale_to_int":
-    #                 method["glob_stats"] = OutputRef(
-    #                     mapped_output_name="glob_stats",
-    #                     method=StatsCalcWrapper(
-    #                         method_repository=MethodRepository,
-    #                         module_path="httomo.methods",
-    #                         method_name="calculate_stats",
-    #                         comm=Comm,
-    #                         save_result=False,
-    #                     ),
-    #                 )
 
     def determine_stages(self) -> Stages:
         """
