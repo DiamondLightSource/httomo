3fd5410ab63d46916dec19eff165b592027dbdf7
algol <dkazanc@hotmail.com>
2025-10-23 16:18:53 +0100

    tests changes2

--- tests/test_parallel_pipeline_big.py
+++ tests/test_parallel_pipeline_big.py
@@ -178,124 +178,6 @@ def test_pipe_parallel_FBP3d_tomobar_k11_38730_in_memory_preview(
 
 # ########################################################################
 
-
-@pytest.mark.full_data_parallel
-def test_parallel_pipe_FBP3d_tomobar_denoising_i13_177906_preview(
-    get_files: Callable,
-    cmd_mpirun,
-    i13_177906,
-    FBP3d_tomobar_denoising,
-    FBP3d_tomobar_TVdenoising_i13_177906_npz,
-    output_folder,
-):
-
-    change_value_parameters_method_pipeline(
-        FBP3d_tomobar_denoising,
-        method=[
-            "standard_tomo",
-        ],
-        key=[
-            "preview",
-        ],
-        value=[
-            {"detector_y": {"start": 900, "stop": 1200}},
-        ],
-    )
-
-    # do not save the result of FBP3d_tomobar
-    change_value_parameters_method_pipeline(
-        FBP3d_tomobar_denoising,
-        method=[
-            "FBP3d_tomobar",
-        ],
-        key=[
-            "recon_size",
-        ],
-        value=[
-            None,
-        ],
-        save_result=False,
-    )
-
-    # change detector_pad value
-    change_value_parameters_method_pipeline(
-        FBP3d_tomobar_denoising,
-        method=[
-            "FBP3d_tomobar",
-        ],
-        key=[
-            "detector_pad",
-        ],
-        value=[
-            100,
-        ],
-    )
-
-    # save the result of denoising instead
-    change_value_parameters_method_pipeline(
-        FBP3d_tomobar_denoising,
-        method=[
-            "total_variation_PD",
-            "total_variation_PD",
-        ],
-        key=[
-            "regularisation_parameter",
-            "iterations",
-        ],
-        value=[
-            1.0e-04,
-            25,
-        ],
-        save_result=True,
-    )
-
-    cmd_mpirun.insert(9, i13_177906)
-    cmd_mpirun.insert(10, FBP3d_tomobar_denoising)
-    cmd_mpirun.insert(11, output_folder)
-
-    process = Popen(
-        cmd_mpirun, env=os.environ, shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE
-    )
-    output, error = process.communicate()
-    print(output)
-
-    files = get_files(output_folder)
-
-    #: check the generated reconstruction (hdf5 file)
-    h5_files = list(filter(lambda x: ".h5" in x, files))
-    assert len(h5_files) == 1
-
-    # load the pre-saved numpy array for comparison bellow
-    data_gt_tv = FBP3d_tomobar_TVdenoising_i13_177906_npz["data"]
-    axis_slice = FBP3d_tomobar_TVdenoising_i13_177906_npz["axis_slice"]
-    (slices, sizeX, sizeY) = np.shape(data_gt_tv)
-
-    step = axis_slice // (slices + 2)
-    # store for the result
-    data_result = np.zeros((slices, sizeX, sizeY), dtype=np.float32)
-
-    path_to_data = "data/"
-    h5_file_name = "total_variation_PD"
-    for file_to_open in h5_files:
-        if h5_file_name in file_to_open:
-            h5f = h5py.File(file_to_open, "r")
-            index_prog = step
-            for i in range(slices):
-                data_result[i, :, :] = h5f[path_to_data][:, index_prog, :]
-                index_prog += step
-            h5f.close()
-        else:
-            message_str = f"File name with {h5_file_name} string cannot be found."
-            raise FileNotFoundError(message_str)
-
-    residual_im = data_gt_tv - data_result
-    res_norm_tv_res = np.linalg.norm(residual_im.flatten()).astype("float32")
-    assert res_norm_tv_res < 1e-5
-
-
-# ########################################################################
-
-
 @pytest.mark.full_data_parallel
 def test_parallel_pipe_LPRec3d_tomobar_i12_119647_preview(
     get_files: Callable,
@@ -504,6 +386,90 @@ def test_parallel_pipe_titaren_center_pc_FBP3d_resample_i12_119647_preview(
     check_tif(files, 200, (512, 512))
     compare_tif(files, files_references)
 
+# ########################################################################
+
+@pytest.mark.full_data_parallel
+def test_pipe_FISTA3d_tomobar_k11_38731(
+    get_files: Callable,
+    cmd_mpirun,
+    diad_k11_38731,
+    FISTA3d_tomobar,
+    FBP3d_tomobar_k11_38731_npz,
+    output_folder,
+):
+    change_value_parameters_method_pipeline(
+        FISTA3d_tomobar,
+        method=[
+            "standard_tomo",
+            "standard_tomo",
+            "standard_tomo",
+            "standard_tomo",
+            "FISTA3d_tomobar",
+            "FISTA3d_tomobar",
+            "FISTA3d_tomobar",
+        ],
+        key=[
+            "preview",
+            "data_path",
+            "image_key_path",
+            "rotation_angles",
+            "iterations",
+            "regularisation_iterations",
+            "regularisation_parameter",
+        ],
+        value=[
+            {"detector_y": {"start": 900, "stop": 1000}},
+            "/entry/imaging/data",
+            "/entry/instrument/imaging/image_key",
+            {"data_path": "/entry/imaging_sum/gts_cs_theta"},
+            10,
+            20,
+            2.0e-07,
+        ],
+    )
+
+    cmd_mpirun.insert(9, diad_k11_38731)
+    cmd_mpirun.insert(10, FISTA3d_tomobar)
+    cmd_mpirun.insert(11, output_folder)
+
+    process = Popen(
+        cmd_mpirun, env=os.environ, shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE
+    )
+    output, error = process.communicate()
+    print(output)
+
+    files = get_files(output_folder)
+
+    #: check the generated reconstruction (hdf5 file)
+    h5_files = list(filter(lambda x: ".h5" in x, files))
+    assert len(h5_files) == 1
+
+    # load the pre-saved numpy array for comparison bellow
+    data_gt = FBP3d_tomobar_k11_38731_npz["data"]
+    axis_slice = FBP3d_tomobar_k11_38731_npz["axis_slice"]
+    (slices, sizeX, sizeY) = np.shape(data_gt)
+
+    step = axis_slice // (slices + 2)
+    # store for the result
+    data_result = np.zeros((slices, sizeX, sizeY), dtype=np.float32)
+
+    path_to_data = "data/"
+    h5_file_name = "FISTA3d_tomobar"
+    for file_to_open in h5_files:
+        if h5_file_name in file_to_open:
+            h5f = h5py.File(file_to_open, "r")
+            index_prog = step
+            for i in range(slices):
+                data_result[i, :, :] = h5f[path_to_data][:, index_prog, :]
+                index_prog += step
+            h5f.close()
+        else:
+            message_str = f"File name with {h5_file_name} string cannot be found."
+            raise FileNotFoundError(message_str)
+
+    residual_im = data_gt - data_result
+    res_norm = np.linalg.norm(residual_im.flatten()).astype("float32")
+    assert res_norm < 1e-6
 
 # ########################################################################
 @pytest.mark.full_data_parallel
