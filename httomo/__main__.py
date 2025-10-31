d2a146ae7a2e41df94fbaa0ecb6a0ba286999b99
Ferenc Tükör <ferenc.tukor@radwaytech.services>
2025-10-31 14:23:14 +0100

    Add option for turning off click's standalone_mode

--- httomo/__main__.py
+++ httomo/__main__.py
@@ -1,4 +1,10 @@
+import sys
 from .cli import main
 
 if __name__ == "__main__":
-    main()
+    standalone_mode = True
+    if "--no-standalone" in sys.argv:
+        standalone_mode = False
+        sys.argv.remove("--no-standalone")
+
+    main(standalone_mode=standalone_mode)
